f75080d11f0298a704f447cd2cc76623e7937bef
Sebastian Schildt <schildt@ibr.cs.tu-bs.de>
2014-08-29 15:02:29 +0200

    Basic TCP Server

--- relaisserver.py
+++ relaisserver.py
@@ -3,8 +3,71 @@
 import relaiscommands as rc
 import serial
 import time
+import socketserver
 
 DEVICE='/dev/ttyUSB0'
+
+
+
+
+
+class RelaisRequestHandler(socketserver.StreamRequestHandler):
+    
+    def __init__(self, request, client_address, server):
+        print('New Request Handler')
+        socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
+        return
+
+    def handle(self):
+        print('Waiting for data...')
+
+        # Echo the back to the client
+        while True:
+            data=self.rfile.readline().strip()
+            if len(data) == 0:
+                break
+
+            print("recv *"+str(data)+"*")
+            if data == bytes("quit","utf-8"):
+                self.request.send(bytes('Bye\n',"utf-8"))
+                break
+            print('recv()->"%s"', data)
+            self.request.send(data)
+            self.request.send(bytes('\n',"utf-8"))
+            
+            
+        return
+
+    def finish(self):
+        print('Request Handler finish')
+        return socketserver.BaseRequestHandler.finish(self)
+
+
+class RelaisServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    
+    def __init__(self, server_address, handler_class=RelaisRequestHandler):
+        print('Init server')
+        socketserver.TCPServer.__init__(self, server_address, handler_class)
+        return
+
+    def serve_forever(self):
+        print('Handling requests, press <Ctrl-C> to quit')
+        while True:
+            self.handle_request()
+        return
+
+    def server_close(self):
+        print('server_close')
+        return socketserver.TCPServer.server_close(self)
+
+    def finish_request(self, request, client_address):
+        print('finish_request(%s, %s)', request, client_address)
+        return socketserver.TCPServer.finish_request(self, request, client_address)
+
+    def close_request(self, request_address):
+        print('close_request(%s)', request_address)
+        return socketserver.TCPServer.close_request(self, request_address)
+
 
 ser = serial.Serial(DEVICE, 19200, timeout=2)
 
@@ -14,6 +77,13 @@ print("Detected boards:")
 for card in cards:
     print("Found card "+str(card['address'])+" with firmware version "+str(card['firmware'])+". Checksum correct?: "+str(card['xorok']))
           
+
+
+print("Starting server")
+address = ('localhost', 2222) # let the kernel give us a port
+server = RelaisServer(address, RelaisRequestHandler)
+server.serve_forever()
+
 
 
 
