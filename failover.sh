9cfb1ba2ab2af2f2f3711cd73f81ad8c11d47aeb
Sebastian Schildt <schildt@ibr.cs.tu-bs.de>
2015-02-17 18:10:55 +0100

    Add script for 3G failover

---
+++ failover.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+TEST_IP=193.99.144.80
+
+
+ping -I eth0 -q -c 2 $TEST_IP > /dev/null  && ETH0UP=1 || ETH0UP=0;
+
+ping -I ppp0 -q -c 2 $TEST_IP > /dev/null  && PPP0UP=2 || PPP0UP=0;
+
+STATE=$(($ETH0UP + $PPP0UP))
+
+
+r=$(ip rule show | head -n 1 | grep cellular | wc -l)
+if [ "$r" -eq "1" ]
+then
+	NOW="ppp"
+else
+	NOW="eth"
+fi
+
+
+echo "FAILOVER: Currently $NOW is active"
+
+echo -n "FAILOVER: Current state is: "
+
+
+case "$STATE" in
+  0   ) echo "BOTH broken"
+        CHOICE="eth"
+        ;;
+  1   ) echo "ETH fine, PPP broken"
+        CHOICE="eth"
+        ;;
+  2   ) echo "ETH broken, PPP fine"
+        CHOICE="ppp"
+       ;;
+  3   ) echo "BOTH fine"
+        CHOICE="eth"
+        ;;
+  *   ) echo "Universe broken"
+       CHOICE="eth"
+       ;;
+esac     
+
+
+if [ "$NOW" != "$CHOICE" ] 
+then
+	echo "FAILOVER: Switching from $NOW to $CHOICE."
+	if [ "$CHOICE" == "ppp" ]
+	then
+		echo ip rule add from all table cellular
+	else
+		echo ip rule del from all table cellular
+	fi
+	echo killall -USR1 autossh
+else
+	echo "FAILOVER: No action taken"
+fi
+
+
